ef39257f8963b6261a08b3148174d117befa9ab7
Gourav Mitra <97431146+Gourav078@users.noreply.github.com>
2024-08-17 21:32:33 +0530

    update

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import UrlShortner from "./url-short/page"
+
+
+export default function Home() {
+  return (
+    <>
+      <UrlShortner />
+    </>
+  );
+}
